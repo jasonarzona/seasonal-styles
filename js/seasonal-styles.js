edffcfae6feef276e1b637b55eb5f283604de4fb
Jason Arjona <jasonmini@Jasons-Mac-mini.lan>
2022-07-28 22:52:20 -0700

    finish

--- js/seasonal-styles.js
+++ js/seasonal-styles.js
@@ -2,7 +2,6 @@ $("document").ready(function(){
     $('.seasons a').click(function(e){  //find all a tags inside class of seasons
         e.preventDefault();  //stop default submission
         var season = $(this).attr("href");  //contents of href attribute of this element
-        // alert(season);
 
 
 
